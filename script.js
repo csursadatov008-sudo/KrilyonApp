184362c491af6dd90174ab0ee2c31f0678dd1445
csursadatov008-sudo <csursadatov008@gmail.com>
2026-09-10 15:21:11 +0400

    Add chat and new chat button event listeners

---
+++ script.js
@@ -0,0 +1,10 @@
+const chat = document.querySelector(".chat");
+const newChatButton = document.querySelector(".new-chat");
+
+chat.addEventListener("click", () => {
+    alert("Söhbət ekranı növbəti mərhələdə hazırlanacaq.");
+});
+
+newChatButton.addEventListener("click", () => {
+    alert("Yeni söhbət funksiyası növbəti mərhələdə hazırlanacaq.");
+});
